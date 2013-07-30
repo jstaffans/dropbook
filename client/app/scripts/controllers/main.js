d921abb4c06e75448d4d296cf56aa73d3385d195
jstaffans <johannes.staffans@gmail.com>
2013-07-30 23:06:25 +0300

    redirects to login page

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -1,17 +1,20 @@
 'use strict';
 
 angular.module('clientApp')
-  .controller('MainCtrl', function ($scope, Restangular, userService) {
+  .controller('MainCtrl', function ($scope, $location, Restangular, userService) {
+    $scope.quoteLoaded = function() {
+      return ($scope.quote !== undefined);
+    }
+
     $scope.getNewQuote = function() {
-      var wisdomPromise = Restangular.one('wisdom', 'random').get();
-      wisdomPromise.then(function (wisdom) {
+      Restangular.one('wisdom', 'random').get().then(function (wisdom) {
         $scope.quote = wisdom.quote;
       });
     };
 
     $scope.logout = function() {
-      userService.logout();
-    };
+      userService.logout().then(function() { $location.path('/login') });
+    }
 
     $scope.getNewQuote();
   });
